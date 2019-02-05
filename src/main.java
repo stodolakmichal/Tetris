23a50e1494562f4af76938f975ea5095bb8e52e5
Michał Stodolak <stodolak.michal@gmail.com>
2019-02-05 22:46:37 +0100

    Commitowanie klasy main

---
+++ src/main.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class main {
+
+    public static void main(String[] args) {
+
+        JFrame obj = new JFrame();
+        obj.setBounds(10,10,700,600);
+        obj.setTitle("Tetris");
+        obj.setResizable(true);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.setVisible(true);
+        obj.setLocationRelativeTo(null);
+
+    }
+}
