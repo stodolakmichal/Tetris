f8ddd76a03d695f7c5e02ed724fd8f80d663a870
Michał Stodolak <stodolak.michal@gmail.com>
2019-02-05 23:43:53 +0100

    xd

--- src/Gameplay.java
+++ src/Gameplay.java
@@ -1,8 +1,53 @@
+package Tetris;
+
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class Gameplay extends JPanel implements KeyListener, ActionListener {
 
+    private boolean play = true;
+    private int score = 0;
 
+    private int delay = 1000;
+    private Timer timer;
+
+    public Gameplay(){
+
+        addKeyListener(this);
+        setFocusable(true);
+        setFocusTraversalKeysEnabled(false);
+        timer = new Timer(delay, this);
+        timer.start();
+    }
+
+    public void paint(Graphics g){
+        //background
+        g.setColor(Color.black);
+        g.fillRect(1,1,692,592);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
 }
--- src/main.java
+++ src/main.java
@@ -1,14 +1,21 @@
+import Tetris.Gameplay;
+
 import javax.swing.*;
 
 public class main {
 
+
+    private static Tetris.Gameplay Gameplay;
+
     public static void main(String[] args) {
 
         JFrame obj = new JFrame();
+        Gameplay = new Tetris.Gameplay();
         obj.setBounds(10,10,700,600);
         obj.setTitle("Tetris");
-        obj.setResizable(true);
+        obj.setResizable(false);
         obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.add(Gameplay);
         obj.setVisible(true);
         obj.setLocationRelativeTo(null);
 
