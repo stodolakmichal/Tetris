93e5135f33a641a2d5d37b77210bbf5a642d5e0c
Michał Stodolak <stodolak.michal@gmail.com>
2019-02-05 23:16:32 +0100

    xxx

--- src/Gameplay.java
+++ src/Gameplay.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
+import javax.swing.*;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyListener;
 
-public class Gameplay {
+public class Gameplay extends JPanel implements KeyListener, ActionListener {
+
+
 }
